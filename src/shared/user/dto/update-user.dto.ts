e87e938e4012486f26eb725eb276ec79465e2338
americano212 <wq0212@naver.com>
2025-01-07 01:50:17 +0900

    feat: add user PW & FCM token update API

--- src/shared/user/dto/update-user.dto.ts
+++ src/shared/user/dto/update-user.dto.ts
@@ -1,4 +1,8 @@
 import { User } from '#entities/user.entity';
 import { PickType } from '@nestjs/swagger';
 
-export class UpdateUserDto extends PickType(User, ['username'] as const) {}
+export class UpdateUserDto extends PickType(User, ['password', 'fcmToken'] as const) {}
+
+export class UpdateUserPasswordDto extends PickType(UpdateUserDto, ['password'] as const) {}
+
+export class UpdateUserFCMDto extends PickType(UpdateUserDto, ['fcmToken'] as const) {}
