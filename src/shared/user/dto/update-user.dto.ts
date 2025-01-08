3a59c3168fcfc43a8d372e1664eb22bf5037d5c5
americano212 <wq0212@naver.com>
2025-01-08 21:53:32 +0900

    feat: add column in user entity about profile image, badge

--- src/shared/user/dto/update-user.dto.ts
+++ src/shared/user/dto/update-user.dto.ts
@@ -1,8 +1,9 @@
 import { User } from '#entities/user.entity';
 import { PickType } from '@nestjs/swagger';
 
-export class UpdateUserDto extends PickType(User, ['password', 'fcmToken'] as const) {}
-
-export class UpdateUserPasswordDto extends PickType(UpdateUserDto, ['password'] as const) {}
-
-export class UpdateUserFCMDto extends PickType(UpdateUserDto, ['fcmToken'] as const) {}
+export class UpdateUserDto extends PickType(User, [
+  'password',
+  'fcmToken',
+  'profileImageCode',
+  'profileBadgeCode',
+] as const) {}
