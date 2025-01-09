c0ea20c3e4becdd339b00339e1e9e5fe2b7c9435
americano212 <wq0212@naver.com>
2025-01-09 15:48:26 +0900

    feat: add notice skeleton

---
+++ src/shared/notice/notice.module.ts
@@ -0,0 +1,16 @@
+import { Global, Module } from '@nestjs/common';
+import { NoticeController } from './notice.controller';
+import * as providers from './providers';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notice } from '#entities/notice.entity';
+
+const services = [...Object.values(providers)];
+
+@Global()
+@Module({
+  imports: [TypeOrmModule.forFeature([Notice])],
+  controllers: [NoticeController],
+  providers: services,
+  exports: services,
+})
+export class NoticeModule {}
