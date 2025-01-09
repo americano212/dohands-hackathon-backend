661d3ec45fb59944ef5227103e44e2b242f144ac
americano212 <wq0212@naver.com>
2025-01-09 22:21:43 +0900

    feat: add user-board repo with set IsRead true

---
+++ src/domain/board/user-board.repository.ts
@@ -0,0 +1,17 @@
+import { Board } from '#entities/board.entity';
+import { UserBoard } from '#entities/user-board.entity';
+import { User } from '#entities/user.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserBoardDto } from './dto';
+
+@Injectable()
+export class UserBoardsRepository {
+  constructor(@InjectRepository(UserBoard) private userBoardsRepository: Repository<UserBoard>) {}
+
+  public async setIsReadTrue(user: User, board: Board): Promise<UserBoard> {
+    const userBoard: CreateUserBoardDto = { user, board, isRead: true };
+    return await this.userBoardsRepository.save(await this.userBoardsRepository.create(userBoard));
+  }
+}
