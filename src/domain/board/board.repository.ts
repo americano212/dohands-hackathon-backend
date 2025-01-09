a3c8800f594d790d6ebc6e31cbda17d5c2d2328c
americano212 <wq0212@naver.com>
2025-01-10 00:07:24 +0900

    fix: change SQL query when find all board API

--- src/domain/board/board.repository.ts
+++ src/domain/board/board.repository.ts
@@ -41,8 +41,13 @@ export class BoardsRepository {
   public async findAllByUserId(userId: number): Promise<Board[]> {
     const boards = await this.boardsRepository
       .createQueryBuilder('board')
-      .leftJoinAndSelect('board.userBoards', 'userBoard', 'userBoard.board_id = board.board_id')
-      .leftJoinAndSelect('userBoard.user', 'user', 'user.user_id = :userId', { userId: userId })
+      .leftJoinAndSelect(
+        'board.userBoards',
+        'userBoard',
+        'userBoard.user_id = :userId', // userId 조건을 ON 절에 추가
+        { userId },
+      )
+      .leftJoinAndSelect('userBoard.user', 'user') // userBoard와 user를 LEFT JOIN
       .getMany();
     return boards;
   }
