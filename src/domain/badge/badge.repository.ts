cef2a9240a9cb90198398e0954434ea9b35795cc
ShinDongJun <wq0212@naver.com>
2025-01-12 15:15:48 +0900

    feat: add test API for give badge to user

--- src/domain/badge/badge.repository.ts
+++ src/domain/badge/badge.repository.ts
@@ -1,4 +1,16 @@
+import { UserBadge } from '#entities/user-badge.entity';
+import { User } from '#entities/user.entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class BadgeRepository {}
+export class BadgeRepository {
+  constructor(@InjectRepository(UserBadge) private userBadgesRepository: Repository<UserBadge>) {}
+
+  public async create(badgeCode: string, user: User): Promise<UserBadge> {
+    return await this.userBadgesRepository.save(
+      await this.userBadgesRepository.create({ badgeCode, user }),
+    );
+  }
+}
--- src/domain/badge/badge.service.ts
+++ src/domain/badge/badge.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/shared/user/user.service';
+import { BadgeRepository } from './badge.repository';
 
 @Injectable()
-export class BadgeService {}
+export class BadgeService {
+  constructor(
+    private readonly user: UserService,
+    private readonly badgesRepository: BadgeRepository,
+  ) {}
+  public async giveBadgeToUser(userId: number, badgeCode: string): Promise<boolean> {
+    const user = await this.user.findOne(userId);
+    const userBadge = await this.badgesRepository.create(badgeCode, user);
+    return userBadge ? true : false;
+  }
+}
