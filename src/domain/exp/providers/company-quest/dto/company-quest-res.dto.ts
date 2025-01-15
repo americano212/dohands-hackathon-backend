df2569e2a09476e65ebb3262bd0038077c8c70f3
김서인 <79834222+seoin0110@users.noreply.github.com>
2025-01-15 15:25:21 +0900

    chore: change company-quest-response dto

--- src/domain/exp/providers/company-quest/dto/company-quest-res.dto.ts
+++ src/domain/exp/providers/company-quest/dto/company-quest-res.dto.ts
@@ -1,5 +1,5 @@
 import { Exp } from '#entities/exp.entity';
-import { PickType } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
 
 //exp 전체 조회할 때 사용
 export class CompanyQuestResponseDto extends PickType(Exp, [
@@ -8,4 +8,7 @@ export class CompanyQuestResponseDto extends PickType(Exp, [
   'questName',
   'content',
   'exp',
-] as const) {}
+] as const) {
+  @ApiProperty({ example: '01.05-01.08', description: '전사 프로젝트 기간' })
+  public override period?: string | null;
+}
