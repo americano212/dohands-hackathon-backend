9065963b9b09c6721f1bf2165d279a6da5894aa3
seoin0110 <seoinseoinseoin@naver.com>
2025-01-16 16:44:20 +0900

    fix: mutex error

--- src/domain/board/board.service.ts
+++ src/domain/board/board.service.ts
@@ -9,9 +9,6 @@ import { Board } from '#entities/board.entity';
 import { Transactional } from 'typeorm-transactional';
 import { NoticeService } from 'src/shared/notice/providers';
 import { SendNoticeDto } from 'src/shared/notice/providers/dto';
-import { Mutex } from 'async-mutex';
-
-const mutex = new Mutex();
 
 @Injectable()
 export class BoardService {
@@ -65,7 +62,6 @@ export class BoardService {
     const tabName = 'notice_board';
     const range = 'C7:D100'; // TODO 레거시
     const values = await this.gssService.getValueFromSheet({ tabName, range });
-
     for (let idx = 0; idx < values.length; idx++) {
       const value = values[idx];
       if (value[0] === '') continue; // No title
@@ -77,31 +73,25 @@ export class BoardService {
         content: value[1],
       };
 
-      const release = await mutex.acquire();
-      try {
-        const isExist = await this.boardsRepository.isExistGoogleSheetId(board.googleSheetId);
+      const isExist = await this.boardsRepository.isExistGoogleSheetId(board.googleSheetId);
 
-        if (isExist) {
-          // ! 있었는데 아예 지워져버리는 경우는 제외됨(삭제 불가)
-          // 이미 존재할 때는 글 내용만 업데이트
-          await this.boardsRepository.updateByGoogleSheetId(board.googleSheetId, {
-            title: board.title,
-            content: board.content,
-          });
-        } else {
-          // 새로운 글일 때는 추가 및 FCM push 알림
-          await this.boardsRepository.create(board);
-          const userIdList = await this.user.findAllUserId(); // 전체 UserId 불러오기
-          const sendNoticeData: SendNoticeDto = {
-            userIdList,
-            title: '새로운 글이 게시되었습니다!',
-            body: '게시판을 통해 글을 확인해 보세요.',
-          };
-          await this.notice.sendNotice(sendNoticeData);
-        }
-      } finally {
-        // 잠금 해제
-        release();
+      if (isExist) {
+        // ! 있었는데 아예 지워져버리는 경우는 제외됨(삭제 불가)
+        // 이미 존재할 때는 글 내용만 업데이트
+        await this.boardsRepository.updateByGoogleSheetId(board.googleSheetId, {
+          title: board.title,
+          content: board.content,
+        });
+      } else {
+        // 새로운 글일 때는 추가 및 FCM push 알림
+        await this.boardsRepository.create(board);
+        const userIdList = await this.user.findAllUserId(); // 전체 UserId 불러오기
+        const sendNoticeData: SendNoticeDto = {
+          userIdList,
+          title: '새로운 글이 게시되었습니다!',
+          body: '게시판을 통해 글을 확인해 보세요.',
+        };
+        await this.notice.sendNotice(sendNoticeData);
       }
     }
 
