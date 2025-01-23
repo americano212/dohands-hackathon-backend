c0190e63320be6bd5682e938cdb8364af914f606
americano212 <wq0212@naver.com>
2025-01-23 22:13:20 +0900

    refactor: erase unused code

--- src/shared/role/role.module.ts
+++ src/shared/role/role.module.ts
@@ -4,14 +4,12 @@ import { UserRole } from '#entities/user-role.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { RoleController } from './role.controller';
 import * as providers from './providers';
 
 const services = Object.values(providers);
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role, UserRole])],
-  controllers: [RoleController],
   providers: services,
   exports: services,
 })
